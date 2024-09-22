7ecd390388032aa19768347a6f1302701bb15fbc
priyankawadle <wadle98priyanka@gmail.com>
2024-09-22 15:20:51 +0530

    simple code kept

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express=require('express');
 const app = express();
-
+require('dotenv').config();
 app.get('/start', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(8080, () => console.log(`http://localhost:8080 app listening on port 8080!`));
+app.listen(process.env.PORT, () => console.log(`http://localhost:${process.env.PORT} app listening on port ${process.env.PORT}!`));
