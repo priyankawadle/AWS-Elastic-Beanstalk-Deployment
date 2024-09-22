ffa1fee2a63dcde3b110890b2a1a6f3f64ac0d26
priyankawadle <wadle98priyanka@gmail.com>
2024-09-22 14:42:03 +0530

    simple code kept

--- app.js
+++ app.js
@@ -1,64 +1,8 @@
 const express=require('express');
-const mysql=require('mysql');
-const bodyParser=require('body-parser');
-require('dotenv').config();
 const app = express();
-app.use(bodyParser.json());
-app.get('/start', (req, res) => {
 
+app.get('/start', (req, res) => {
     res.send('Hello World!');
 });
 
-const con = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    port:process.env.MYSQLPORT
-});
-
-con.connect(function(err) {
-    if(err) throw err;
-    console.log('Connected');  
-});
-
-
-app.get('/api/items', (req, res) => {
-    const query='select * from customers';
-    con.query(query,(err,result)=>{
-        if(err) throw err;
-        console.log(result);
-        res.send({status:200,data:result});
-    })
-});
-
-app.post('/api/items', (req, res) => {
-    const body=req.body;
-    const query='insert into customers (name,email) values ("'+body.name+'","'+body.email+'")';
-    con.query(query,(err,result)=>{
-        if(err) throw err;
-        console.log(result);
-        res.send({status:200,data:result});
-    })
-});
-
-app.put('/api/items/:id', (req, res) => {
-    const body=req.body;
-    const query='update customers set name="'+body.name+'",email="'+body.email+'" where id="'+req.params.id+'"';
-    con.query(query,(err,result)=>{
-        if(err) throw err;
-        console.log(result);
-        res.send({status:200,data:result});
-    })
-});
-
-app.delete('/api/items/:id', (req, res) => {
-    const query='delete from customers where id="'+req.params.id+'"';
-    con.query(query,(err,result)=>{
-        if(err) throw err;
-        console.log(result);
-        res.send({status:200,data:result});
-    })
-});
-
-app.listen(process.env.PORT, () => console.log(`http://localhost:${process.env.PORT} app listening on port ${process.env.PORT}!`));
+app.listen(3000, () => console.log(`http://localhost:3000 app listening on port 3000!`));
