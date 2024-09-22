f42c56fb8fda89613b4fe3520b9c8abc6b271870
priyankawadle <wadle98priyanka@gmail.com>
2024-09-22 15:24:38 +0530

    MYSQL CONNECTION ADDED

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const express=require('express');
-const app = express();
+const mysql=require('mysql');
+const bodyParser=require('body-parser');
 require('dotenv').config();
+const app = express();
+app.use(bodyParser.json());
+app.get('/start', (req, res) => {
+
+    res.send('Hello World!');
+});
+
+const con = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port:process.env.MYSQLPORT
+});
+
+con.connect(function(err) {
+    if(err) throw err;
+    console.log('Connected');  
+});
+
 app.get('/start', (req, res) => {
     res.send('Hello World!');
 });
